c2cd58f7dd7a8b62d92dc364d82adbd7d6b072a0
M <markakinshev@gmail.com>
2018-08-10 20:46:57 -0500

    main src
    contains dictionary and function addChar

---
+++ main.js
@@ -0,0 +1,10 @@
+var dict = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x","y, "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; //list of char for random discord username generation
+
+funtion addChar () {
+	for (var i = 0; i < dict.length; i++) {
+		var username += dict[i] + "";
+	}
+};
+
+addChar();
+
